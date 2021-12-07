37671bcbd4f1133693e416bc816ae354d3f2359a
Maurice Ainon <maurice.ainon@capgemini.com>
2021-12-07 19:58:58 +0000

    Finish custom filters

---
+++ blog/templatetags/blog_extras.py
@@ -0,0 +1,23 @@
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
+from django.utils.html import format_html
+
+@register.filter
+def author_details(author):
+    if not isinstance(author, user_model):
+        # return empty string as safe default
+        return ""
+
+    if author.first_name and author.last_name:
+        name = f"{author.first_name} {author.last_name}"
+    else:
+        name = f"{author.username}"
+
+    if author.email:
+        prefix = format_html('<a href="mailto:{}">', author.email)
+        suffix = format_html("</a>")
+    else:
+        prefix = ""
+        suffix = ""
+
+    return format_html('{}{}{}', prefix, name, suffix)
