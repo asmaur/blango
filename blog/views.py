e9680e511afcb59f726318e86b79180f2bdf5a45
Maurice Ainon <maurice.ainon@capgemini.com>
2022-01-03 19:53:58 +0000

    Finish fetch and React hooks

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
 from blog.forms import CommentForm
+from django.urls import reverse
 
 
 # Create your views here.
@@ -36,4 +37,6 @@ def get_ip(request):
   return HttpResponse(request.META['REMOTE_ADDR'])
 
 def post_table(request):
-    return render(request, "blog/post-table.html")
+    return render(
+        request, "blog/post-table.html", {"post_list_url": reverse("post-list")}
+    )
